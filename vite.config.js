f4c2d473079fd7da15586d177065c5e423410a47
Sawan Rai <raisawan800@gmail.com>
2026-05-30 13:14:42 +0530

    Fix Image Appearance Issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
+const isProd = process.env.NODE_ENV === 'production'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base:"/DiceGame",
+  plugins: [react(), tailwindcss()],
+  base: isProd ? '/DiceGame/' : '/',
 })
